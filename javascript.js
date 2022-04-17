f0ad64f8a91c6a02d6a62910ba3237fbc88699c4
Eric Petela <epetela51@gmail.com>
2022-04-17 07:25:43 -0400

    Updated HTML & JS
    Added button in HTML to change grid size
    Added JS to accompany HTML button and started creating function to resize grid on button click

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 // Code for populating individual boxes & grid
 let container = document.querySelector('#grid-container')
-let resetBtn = document.querySelector('#reset')
 
 function box() {
     let square = document.createElement('div')
@@ -21,8 +20,10 @@ addMultipleBoxes()
 
 
 //////////////////////////////////////////////////////////////////////////////
-
 // Code for highlighting boxes with mouseover & resetting grid with button click
+
+// reset button
+let resetBtn = document.querySelector('#reset')
 
 // grabs all divs with the class of box and creates a nodeList
 let boxx = document.querySelectorAll('.box')
@@ -51,8 +52,7 @@ resetBtn.addEventListener('click', () => {
 
 
 ///////////////////////////////////////////////////////////////////////////
-
-// Code for slider
+// Code for slider & changing grid size
 
 let slider = document.querySelector('#sliderRange')
 let outputSliderValue = document.querySelector('#sliderValue')
@@ -66,4 +66,17 @@ slider.oninput = updateSliderValue
 
 function updateSliderValue(e) {
     outputSliderValue.textContent = `${e.target.value} x ${e.target.value}`
+}
+
+// btn to change the grid size
+let newGridBtn = document.querySelector('#newGrid')
+
+newGridBtn.addEventListener('click', newGrid)
+
+// creates new grid size
+function newGrid() {
+    let currentValue = slider.value
+    console.log(currentValue)
+
+    container.style.gridTemplateColumns = repeat(currentValue, auto);
 }
